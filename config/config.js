f1682c3328bc229673c5b96b67342db30a21686b
any.okulova <any-okulova@yandex.ru>
2022-02-27 22:57:10 +0300

    init

---
+++ config/config.js
@@ -0,0 +1,5 @@
+const config = {
+    app_name: 'Gif search',
+    giphy_apy_key: 'tMZbDeHHs2Al1rnckeagj302dXLSlgv0',
+    giphy_url_search: 'https://api.giphy.com/v1/gifs/search'
+}
---
+++ js/script.js
@@ -0,0 +1,9 @@
+let data = {
+    api_key: 'tMZbDeHHs2Al1rnckeagj302dXLSlgv0',
+    q: 'test'
+};
+
+// fetch('https://api.giphy.com/v1/gifs/search?' + new URLSearchParams(data))
+//     .then((data) => {
+//         console.log(data);
+//     });
